dee53803deac028200a384209826e97f336740f9
SambeetKumar <khatuasambeet31@gmail.com>
2022-10-29 17:34:39 +0530

    corrected

--- codes/HelloWorld/src/demo.java
+++ codes/HelloWorld/src/demo.java
@@ -1,12 +1,5 @@
 public class demo {
     public static void main(String[] args){
-        int n = 30;
-        if(n%5 == 0 && n%3 == 0)
-        {
-            n +=n;
-            System.out.println(++n+" ");
-        }
-        n++;
-        System.out.println(++n+" "+"bye");
+        System.out.print("Hello world!");
     }
 }
